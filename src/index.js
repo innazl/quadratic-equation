e2e93686e214b8e0e6db621a75152f74afa98b9d
innazl <mozyr.innazl@tut.by>
2018-09-26 23:08:26 +0300

    quadratic-equation: task is done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  const array = equation.split(' ');
+  let a = Number(array[0]);
+  let b = Number(array[3] + array[4]);
+  let c = Number(array[7] + array[8]);
+  let d = b * b - 4 * a * c;
+  const result = [ ];
+  if (d > 0) {
+    result.push(Math.round((-b + Math.sqrt(d)) / (2*a)));
+    result.push(Math.round((-b - Math.sqrt(d)) / (2*a)));
+  } else if (d = 0) {
+    result.push(Math.round(-b / (2*a)));
+  }
+  return result.sort((a, b) => a - b);
 }
